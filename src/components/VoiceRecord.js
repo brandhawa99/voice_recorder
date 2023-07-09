7e408b4e790680801cf7ee2bdc7775bd756d7cfc
brandhawa99 <baltej.randhawa0@gmail.com>
2023-07-08 23:12:06 -0700

    added audio playback functionality

--- src/components/VoiceRecord.js
+++ src/components/VoiceRecord.js
@@ -1,9 +1,11 @@
-import React,{useState} from "react";
+import React,{useEffect, useState} from "react";
 import {View, Text, StyleSheet, SafeAreaView, Button} from 'react-native';
 import {Audio} from 'expo-av'
 
 const VoiceRecord = () =>{
+  const [sound, setSound] = useState(); 
   const [recording, setRecording] = useState()
+  const [URI, setURI] = useState(null); 
   const startRecording  = async() =>{
     try {
       await Audio.requestPermissionsAsync();
@@ -28,14 +30,37 @@ const VoiceRecord = () =>{
       allowsRecordingIOS:false,
     })
     const uri = recording.getURI();
+    setURI(uri);
     console.log("Recording stopped and stored at", uri);
   }
+  const playSound = async() =>{
+    try{
+      console.log("loading sound"); 
+      const {sound} = await Audio.Sound.createAsync(require("./assets/music/1.mp4")); 
+      setSound(sound); 
+      await sound.playAsync(); 
+    }catch(e){
+      console.log(e);
+    }
+  } 
+
+  useEffect(()=>{
+    return sound
+      ? () => {
+          console.log('Unloading Sound');
+          sound.unloadAsync();
+        }
+      : undefined;
+  },[sound])
 
   return(
     <SafeAreaView style={styles.container}>
         <Text>Record</Text>
         <View style={styles.button}>
           <Button style={styles.recordButton} title={recording? "Stop Recording":"Start Recording"} onPress={recording?stopRecording:startRecording}/>
+        </View>
+        <View>
+          <Button title="Play Sound" onPress={playSound} />
         </View>
     </SafeAreaView>
   )
