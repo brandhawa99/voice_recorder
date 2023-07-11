012bf25f773a5ca4ef591be81078aa74987b5d8c
brandhawa99 <baltej.randhawa0@gmail.com>
2023-07-10 20:00:18 -0700

    added camera component to app

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import Header from './src/components/Header'
 import VoiceRecord from './src/components/VoiceRecord';
+import CameraView from './src/components/Camera';
 export default function App() {
   return (
     <View style={styles.container}>
         <Header />
         <VoiceRecord />
+        <CameraView />
       </View>
   );
 }
